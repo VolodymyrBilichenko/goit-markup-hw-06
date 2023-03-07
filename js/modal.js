93613b0be843fa1f18f7a35e26fe3109f3a21178
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2023-03-07 17:09:52 +0200

    update

--- js/modal.js
+++ js/modal.js
@@ -3,10 +3,16 @@
     openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
+    modalCheckbox: document.querySelector("[data-modal-checkbox]"),
+    submeitModalBtn: document.querySelector("[data-modal-submit]"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
+  // active btn depens of check status
+  refs.modalCheckbox.addEventListener("click", () => {
+    refs.submeitModalBtn.toggleAttribute('disabled')
+  })
 
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
